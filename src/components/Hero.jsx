e070264259cab83462edb1ac261af9aed5e4d8e3
VIEN HONG <vienhong88@gmail.com>
2023-05-01 13:15:09 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz-logo"
+        className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open('https://github.com/vienhong20/AI_Summarizer')}
+          className="black_btn"
+          >
+          GitHub
+        </button>
+
+      </nav>
+    </header>
   )
 }
 
