65098f9dd3ffba35aeb44e979c7dbea2f1698d00
VIEN HONG <vienhong88@gmail.com>
2023-05-02 13:25:20 -0400

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,22 @@ import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const handleSubmit = async (e) => {
+    alert('Submitted');
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form 
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
           >
             <img 
               src={linkIcon}
@@ -18,10 +27,30 @@ const Demo = () => {
             />
 
             <input 
+              type="url"
+              placeholder="Enter a URL"
+              value={article.url}
+              onChange={(e) => setArticle({...article, url: e.target.value })}
+              required
+              className="url_input peer"
             />
 
+            <button
+              type="submit"
+              className="submit_btn 
+                peer-focus:border-gray-700
+                peer-focus:text-gray-700"
+            >
+              <p>↵</p>
+            </button>
         </form>
+
+        {/* Browser URL History */}
+
       </div>
+
+      {/* Display Results */}
+
     </section>
   )
 }
