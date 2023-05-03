0574700845138d8a4e190022c35666a8656fb0b4
VIEN HONG <vienhong88@gmail.com>
2023-05-03 14:29:18 -0400

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,79 +1,96 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { copy, linkIcon, loader, tick } from '../assets';
-import { useLazyGetSummaryQuery } from '../services/article';
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary: '',
+    url: "",
+    summary: "",
   });
-
   const [allArticles, setAllArticles] = useState([]);
-  
+  const [copied, setCopied] = useState("");
 
-  const[getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  // Load data from localStorage on mount
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
-    )
+      localStorage.getItem("articles")
+    );
 
-    if(articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl: article.url });
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
 
-    if(data?.summary) {
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
 
+      // update state and local storage
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
+
+  // copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
-      <div className="flex flex-col w-full gap-2">
-        <form 
-          className="relative flex justify-center items-center"
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
           onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
+            required
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
           >
-            <img 
-              src={linkIcon}
-              alt="link_icon"
-              className="absolute left-0 my-2 ml-3 w-5"
-            />
-
-            <input 
-              type="url"
-              placeholder="Enter a URL"
-              value={article.url}
-              onChange={(e) => setArticle({...article, url: e.target.value })}
-              required
-              className="url_input peer"
-            />
-
-            <button
-              type="submit"
-              className="submit_btn 
-                peer-focus:border-gray-700
-                peer-focus:text-gray-700"
-            >
-              <p>↵</p>
-            </button>
+            <p>↵</p>
+          </button>
         </form>
 
-        {/* Browser URL History */}
+        {/* Browse History */}
         <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
           {allArticles.reverse().map((item, index) => (
             <div
@@ -83,8 +100,8 @@ const Demo = () => {
             >
               <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
-                  alt="copy_icon"
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
                   className='w-[40%] h-[40%] object-contain'
                 />
               </div>
@@ -96,7 +113,7 @@ const Demo = () => {
         </div>
       </div>
 
-      {/* Display Results */}
+      {/* Display Result */}
       <div className='my-10 max-w-full flex justify-center items-center'>
         {isFetching ? (
           <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
@@ -124,7 +141,7 @@ const Demo = () => {
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
